6d38748988f07e643644555bf98cf4e3710e07c4
rj_sathusan <www.rjsathusan@gmail.com>
2025-05-22 20:26:03 +0530

    Night 03

--- DevPro.Api/Controllers/ProductController.cs
+++ DevPro.Api/Controllers/ProductController.cs
@@ -6,7 +6,7 @@ using Microsoft.Extensions.Logging;
 namespace DevPro.Api.Controllers
 {
     [ApiController]
-    [Route("api/productsTest02")]
+    [Route("api/product")]
     public class ProductController : ControllerBase
     {
         private readonly IProductService _productService;
