1e6025e9b8e02c77d34b1b1952c35155345c4b4c
rj_sathusan <www.rjsathusan@gmail.com>
2025-05-22 16:00:00 +0530

    JENKIN AUTO TEST 2

--- DevPro.Api/Controllers/ProductController.cs
+++ DevPro.Api/Controllers/ProductController.cs
@@ -6,7 +6,7 @@ using Microsoft.Extensions.Logging;
 namespace DevPro.Api.Controllers
 {
     [ApiController]
-    [Route("api/products")]
+    [Route("api/test")]
     public class ProductController : ControllerBase
     {
         private readonly IProductService _productService;
